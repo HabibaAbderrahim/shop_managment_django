bdc970e1352197b3e6c60476f69bbc3f81986993
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-26 16:28:26 +0100

    setting.py

--- Shop/settings.py
+++ Shop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'client',
+    'order',
+    'product'
 ]
 
 MIDDLEWARE = [
