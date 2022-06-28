920424df2dfde3e7bb1d8d1c1959d998101b9f56
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-28 14:35:26 +0100

    serving static filews

--- Shop/urls.py
+++ Shop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,5 @@ urlpatterns = [
     path('order/', include('order.urls'))
 
 ]
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
