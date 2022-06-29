8c2bc45c654ce1c038ac7beb6c9cd3e47ba9baff
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-29 17:42:13 +0100

    order  model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,13 @@
+from http import client
+from itertools import product
 from django.db import models
 
 # Create your models here.
+
+class Order (models.Model):
+    status_list=(("in production","in production"),("Shipped","Shipped"),("Delivered","Delivered"))
+    # client =
+    # product =
+    #choices 
+    status = models.CharField(null=True, max_length=200,choices=status_list)
+    date_create= models.DateTimeField(null=True, auto_now_add=True)
