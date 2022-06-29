a8734adafdedb714f2dbd7d94315fccc34ec60e4
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-29 17:27:56 +0100

    Product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name=models.CharField(max_length=200 , null = True)
+    price = models.FloatField(null=True)
+
