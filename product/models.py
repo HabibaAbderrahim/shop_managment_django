d1d8d235339ca28c2fc33403f03cfd310b636608
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-30 17:07:47 +0100

    add relations many to many

--- product/models.py
+++ product/models.py
@@ -1,12 +1,14 @@
+import imp
 from django.db import models
-
+from tag.models import Tag
 # Create your models here.
 
 class Product(models.Model):
     name=models.CharField(max_length=200 , null = True)
     price = models.FloatField(null=True)
     date_create = models.DateTimeField(null=True, auto_now_add=True)
-
+    #many to many 
+    tag=models.ManyToManyField(Tag)
     def __str__(self):
         return self.name
     
