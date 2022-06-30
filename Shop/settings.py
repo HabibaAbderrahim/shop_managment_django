4836711e7e3aba3210a2326e6827278839d12162
HabibaAbderrahim <heububabiba@gmail.com>
2022-06-30 16:59:15 +0100

    add tag

--- Shop/settings.py
+++ Shop/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'client',
     'order',
-    'product'
+    'product',
+    'tag'
 ]
 
 MIDDLEWARE = [
--- order/models.py
+++ order/models.py
@@ -15,3 +15,7 @@ class Order (models.Model):
     #choices 
     status = models.CharField(null=True, max_length=200,choices=status_list)
     date_create= models.DateTimeField(null=True, auto_now_add=True)
+
+    def __str__(self):
+        return (self.product,self.status)
+    
