a856b5dd84750c19f75b76532e7b48d3541d459c
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-01 16:13:39 +0100

    products

--- product/views.py
+++ product/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from client.models import Client
 from order.models import Order
+from product.models import Product
 # Create your views here.
 
 def home(request):
     #findall from model
     clients= Client.objects.all()
     orders=Order.objects.all()
+    products=Product.objects.all()
     #dict key value
-    context={'clients':clients,'orders':orders}
+    context={'clients':clients,'orders':orders,'product':products}
 
     #render template with data
     return render(request ,'product\home.html', context)
