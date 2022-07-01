77aace820d4470507d788968a24b407681909d64
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-01 17:41:15 +0100

    list client

--- client/views.py
+++ client/views.py
@@ -1,7 +1,10 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from order.models import Order
 from .models import Client
+from order.models import Order
 # Create your views here.
 
 #this render a commun template for all without id client
@@ -10,6 +13,9 @@ from .models import Client
 
 #pk to get client id with him details
 def list_client(request ,pk):
+    #get client with that specific key
     clientKey=Client.objects.get(id=pk)
-    context ={'client':clientKey}
+    #orders of specific client
+    ordersList=clientKey.order_set.all()
+    context ={'clientX':clientKey ,'ordersList':ordersList}
     return render(request ,'client\list_client.html',context)
