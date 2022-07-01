2202bd8117d790421406da822f70f8b35ca4cd1e
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-01 18:41:56 +0100

    list client

--- client/views.py
+++ client/views.py
@@ -1,3 +1,4 @@
+from itertools import product
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -5,6 +6,7 @@ from django.shortcuts import render
 from order.models import Order
 from .models import Client
 from order.models import Order
+from product.models import Product
 # Create your views here.
 
 #this render a commun template for all without id client
@@ -15,7 +17,9 @@ from order.models import Order
 def list_client(request ,pk):
     #get client with that specific key
     clientKey=Client.objects.get(id=pk)
-    #orders of specific client
+    #orders of specific client:client product
     ordersList=clientKey.order_set.all()
-    context ={'clientX':clientKey ,'ordersList':ordersList}
+    #total
+    total = ordersList.count()
+    context ={'clientX':clientKey ,'ordersList':ordersList,'total':total}
     return render(request ,'client\list_client.html',context)
