653ddca9369957775ba46a493b9aa1a9f5263296
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-10 15:18:57 +0100

    add tags

--- product/views.py
+++ product/views.py
@@ -1,5 +1,7 @@
 from http import client
+from msilib import Table
 from multiprocessing import context
+import string
 from django.http import HttpResponse
 from django.shortcuts import render
 from client.models import Client
@@ -8,21 +10,25 @@ from product.models import Product
 from tag.models import Tag
 # Create your views here.
 
-def home(request):
+def home(request ):
+    pk=1
     #findall from model
     clients= Client.objects.all()
     orders=Order.objects.all()
     products=Product.objects.all()
+    #filtring
+    tags=Product.objects.get(id=pk).tag.all()
     #dict key value
-    context={'clients':clients,'orders':orders,'product':products}
+    context={'clients':clients,'orders':orders,'product':products,'tags':tags}
 
     #render template with data
     return render(request ,'product\home.html', context)
 
 def detail(request , pk) :
     productX = Product.objects.get(id=pk)
-    #tag 
-    tagX =Tag.objects.get(id=productX.id)
+    #filter
+
+    tagX =Product.objects.get(id=pk).tag.all()
     context ={'productx':productX , 'tagx':tagX}
 
     return render(request,'product\single.html',context)   
