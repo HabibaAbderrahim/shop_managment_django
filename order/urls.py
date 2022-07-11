fe48303da137a971d55a133e09ae6f2def5103d7
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-11 17:40:54 +0100

    modify order

--- order/urls.py
+++ order/urls.py
@@ -7,6 +7,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_order),
-    path('add_order', views.add_order,name="add_order")
+    path('add_order', views.add_order,name="add_order"),
     path('modify_order/<str:pk>', views.modify_order , name="modify_order")
 ]
