c5f6eafca4bd7f5d3264f5924d79acebe035438d
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-11 17:19:32 +0100

    add url

--- order/urls.py
+++ order/urls.py
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path('', views.list_order),
     path('add_order', views.add_order,name="add_order")
+    path('modify_order/<str:pk>', views.modify_order , name="modify_order")
 ]
