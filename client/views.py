c076f55194ec42c5ead3dff3173a648132c95670
HabibaAbderrahim <heububabiba@gmail.com>
2022-07-15 23:17:25 +0100

    client filter view

--- client/views.py
+++ client/views.py
@@ -1,3 +1,4 @@
+import imp
 from itertools import product
 from multiprocessing import context
 from django.http import HttpResponse
@@ -9,6 +10,7 @@ from order.models import Order
 from product.models import Product
 from .forms import ClientForm
 from product.views import manage
+from .filters import ClientFilter
 # Create your views here.
 
 #this render a commun template for all without id client
@@ -18,12 +20,16 @@ from product.views import manage
 #pk to get client id with him details
 def list_client(request ,pk):
     #get client with that specific key
-    clientKey=Client.objects.get(id=pk)
+    clientKey=Client.objects.get(id=pk) 
     #orders of specific client:client product
     ordersList=clientKey.order_set.all()
+    #filter
+    filterClient= ClientFilter(request.GET , queryset=ordersList)
+    #update List
+    ordersList = filterClient.qs
     #total
     total = ordersList.count()
-    context ={'clientX':clientKey ,'ordersList':ordersList,'total':total}
+    context ={'clientX':clientKey ,'ordersList':ordersList,'total':total , 'filterClient':filterClient}
     return render(request ,'client\list_client.html',context)
 
 def add_client(request):
